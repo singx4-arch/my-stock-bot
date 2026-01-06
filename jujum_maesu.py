19d85ae224c2d467f8ce57141b1a9f50b454fd7b
singx4-arch <singx4@gmail.com>
2026-01-06 17:26:45 +0900

    Update jujum_maesu.py

--- jujum_maesu.py
+++ jujum_maesu.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 # 1. 환경 설정 및 세션 관리이다
 token = '8160201188:AAELStlMFcTeqpFZYuF-dsvnXWppN7iOHiI' 
-chat_id = '-1004998189045'
+chat_id = '-4998189045'
 SENT_ALERTS_FILE = 'sent_alerts.json'
 
 def send_message(text):
